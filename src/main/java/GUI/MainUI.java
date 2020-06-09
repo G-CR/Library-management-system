8f6180ca8dd7c27783dd49378acb59af70444a0a
GongCR <47514055+O-Bus@users.noreply.github.com>
2020-06-09 15:42:46 +0800

    写了一下文档

--- src/main/java/GUI/MainUI.java
+++ src/main/java/GUI/MainUI.java
@@ -1,7 +1,31 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainUI {
     public MainUI() {
-        
+        JFrame f = new JFrame();
+        JPanel p = new JPanel();
+
+        JLabel title = new JLabel("图书借阅系统");
+
+        // 查找书籍的板板
+        JPanel p1 = new JPanel();
+        JLabel tit1 = new JLabel("查找书籍 ");
+        String[] s = {}
+
+        p.add(title);
+
+        GridLayout fl = new GridLayout();
+        f.setLayout(fl);
+        f.getContentPane().add(p);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(350, 350);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainUI();
     }
 }
