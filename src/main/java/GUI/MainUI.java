965c5400162c9b597039a529ed3d83e8a88d5cc2
GongCR <47514055+O-Bus@users.noreply.github.com>
2020-06-10 13:32:32 +0800

    显示书籍列表 并且提供根据条件查询的功能

--- src/main/java/GUI/MainUI.java
+++ src/main/java/GUI/MainUI.java
@@ -1,9 +1,18 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.lang.reflect.Type;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
 
 public class MainUI {
+    JTable table;
+    JScrollPane sc;
     public MainUI() {
         JFrame f = new JFrame();
         JPanel p = new JPanel();
@@ -32,13 +41,89 @@ public class MainUI {
         JButton show = new JButton("已借阅书籍");
         p3.add(find);p3.add(show);
 
-        p.add(title); p.add(p1); p.add(p2); p.add(p3);
+        // 显示对话框下面的书籍信息
+        con_sql conSql = new con_sql();
+        Connection conn = conSql.getConn();
+        String sql = "SELECT ISBN, Book_name, Author, Price, Press, Date_of_publication, Inventory, ty.type_name FROM Book, Types_of_books ty WHERE ty.type_num = Book.Type";
+        String[] columnNames = {"ISBN编号","书名","作者","单价","出版社","出版日期","库存","书籍种类"};
+        DefaultTableModel dtm = new DefaultTableModel();
+        dtm.setColumnIdentifiers(columnNames); // 给书籍信息加上基础信息
+
+        PreparedStatement pstmt;
+        try {
+            pstmt = (PreparedStatement) conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                Vector v = new Vector();
+                v.add(rs.getString("ISBN"));
+                v.add(rs.getString("Book_name"));
+                v.add(rs.getString("Author"));
+                v.add(rs.getString("Price"));
+                v.add(rs.getString("Press"));
+                v.add(rs.getString("Date_of_publication"));
+                v.add(rs.getString("Inventory"));
+                v.add(rs.getString("ty.type_name"));
+
+                dtm.addRow(v);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        table = new JTable(dtm);
+        sc = new JScrollPane(table);
+
+        // 添加查找按钮监听器
+        find.addActionListener( e -> {
+            String find_type = (String) type.getSelectedItem();
+            String input = in_type.getText();
+            int low = Integer.valueOf(price1.getText());
+            int high = Integer.valueOf(price2.getText());
+
+            if(find_type.equals("书籍名称")) find_type = "Book_name";
+            else if(find_type.equals("ISBN编号")) find_type = "ISBN";
+            else if(find_type.equals("书籍种类")) find_type = "Type";
+            else if(find_type.equals("出版社")) find_type = "Press";
+            else if(find_type.equals("作者")) find_type = "Author";
+
+
+            String sql_ = "SELECT ISBN, Book_name, Author, Price, Press, Date_of_publication, Inventory, ty.type_name FROM Book, Types_of_books ty WHERE ty.type_num = Book.Type AND (Price BETWEEN " + low + " AND " + high+ ") AND " + find_type + " LIKE '%" + input + "%'";
+            DefaultTableModel dtm1 = new DefaultTableModel();
+            dtm1.setColumnIdentifiers(columnNames); // 给书籍信息加上基础信息
+            PreparedStatement pstmt1;
+            try {
+                pstmt1 = (PreparedStatement) conn.prepareStatement(sql_);
+                ResultSet rs = pstmt1.executeQuery();
+                while(rs.next()) {
+                    Vector v = new Vector();
+                    v.add(rs.getString("ISBN"));
+                    v.add(rs.getString("Book_name"));
+                    v.add(rs.getString("Author"));
+                    v.add(rs.getString("Price"));
+                    v.add(rs.getString("Press"));
+                    v.add(rs.getString("Date_of_publication"));
+                    v.add(rs.getString("Inventory"));
+                    v.add(rs.getString("ty.type_name"));
+
+                    dtm1.addRow(v);
+                }
+            }
+            catch (SQLException g) {
+                g.printStackTrace();
+            }
+            table.setModel(dtm1);
+        });
+        /*-------------------------------------------------------------------------*/
+
+
+        p.add(title); p.add(p1); p.add(p2); p.add(p3); p.add(sc);
 
         GridLayout fl = new GridLayout();
         f.setLayout(fl);
         f.getContentPane().add(p);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(450, 350);
+        f.setSize(450, 450);
         f.setVisible(true);
     }
 }
